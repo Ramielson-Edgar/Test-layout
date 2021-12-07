35c7e881c6c95f8ba9318306640e46228f721f5d
designeradro <77737840+designeradro@users.noreply.github.com>
2021-12-07 19:31:20 +0200

    almost done

--- js/main.js
+++ js/main.js
@@ -1,67 +1,76 @@
 const allSliders =document.querySelectorAll('.copy-how-it-work-item');
 const sliderList = document.querySelector('.copy-how-it-work-list')
-
-let offset = 0
-  
-document.querySelector('.next-slide').addEventListener('click', ()=> {
-    offset = offset + 277.5;
-
-    for (const slide of allSliders) {
+const animationContainers = document.querySelectorAll('.animation-container')
  
-        slide.addEventListener('click', ()=> {
+
+
+// SLIDER
+// смешения
+let offset = 0
+ 
+// Управления слайдером вперёд
+document.querySelector('.next-slide').addEventListener('click', ( )=> {
+     offset += 458;
+
         
-        
+        if(offset > 1374) {
+            offset = 0
+            sliderList.style.left = offset;
+            return
+        }
+
+        sliderList.style.left = -offset + 'px';
+        document.querySelector('.next-slide p').classList.add('active-btn')
+        document.querySelector('.previously-slide p').classList.remove('active-btn')
+})
+
+ 
+// Управления слайдером назад
+ document.querySelector('.previously-slide').addEventListener('click', ()=> {
+        offset -= 458;
+
+
+        if(offset < 0)  {
+             offset = 1374;
+             sliderList.style.left = offset;
+             return
+        }
+    
+        sliderList.style.left = -offset + 'px';
+        document.querySelector('.next-slide p').classList.remove('active-btn')
+        document.querySelector('.previously-slide p').classList.add('active-btn')
+})
+    
+    
+// По клику вешаю активный класс 
+    for (const slide of allSliders) {
+
+        slide.addEventListener('click', (event)=> {
+        clearActiveClass()
+
+        slide.classList.add('active')
+   
+
+        // if( event.target !== animationContainers) {
+        // offset+=277.5;
+        // sliderList.style.left = -offset + 'px';
+         
+        // }
+
+
+        // if(offset > 832.5) {
+        // offset = 0
+        // sliderList.style.left = offset;
+        // return
+        // }
+
        })
     
     
     }
-
- 
-    if(offset > 1374) {
-        offset = 0
-        sliderList.style.left = offset;
-    }
-
-
-    sliderList.style.left = -offset + 'px';
-    document.querySelector('.next-slide p').classList.add('active-btn')
-    document.querySelector('.previously-slide p').classList.remove('active-btn')
-
-})
-
-
-document.querySelector('.previously-slide').addEventListener('click', ()=> {
-    offset = offset - 458;
     
-    if(offset < 0)  {
-         offset = 1374;
-         sliderList.style.left = offset;
-    }
 
-    sliderList.style.left = -offset + 'px';
-    document.querySelector('.next-slide p').classList.remove('active-btn')
-    document.querySelector('.previously-slide p').classList.add('active-btn')
-})
-
-
-for (const slide of allSliders) {
- 
-    slide.addEventListener('click', ()=> {
-       clearActiveClass()
-   
-        
-     slide.classList.toggle('active')
-    //  slide.classList.toggle('non-active')
-    
-   })
-
-
-}
-
-
-
- 
-
+// Сбрасываюс события активного слайда
 function clearActiveClass() {
     allSliders.forEach((slide)=> {
         slide.classList.remove('active')
@@ -70,10 +79,27 @@ function clearActiveClass() {
 }
 
 
-
+// SHOW ANIMATION BAR
+function showAnimationBar () {
+    for(const animationContainer of animationContainers ) {
+        animationContainer.addEventListener('click', (event)=> {
+  
+         
+                animationContainer.classList.toggle('show-vid')
+                animationContainer.classList.toggle('hide-vid')
+               
+            
+                   
+                })
+    }
  
+}
+
+ showAnimationBar ()
 
 
+
+// ANIMATION 
 const slideUp = {
     distance: '30%',
     origin: 'bottom',
@@ -81,9 +107,6 @@ const slideUp = {
     delay: 300,
     opacity: 0
 };
-
-
-
 
 
 ScrollReveal().reveal('.copy-hero-section .copy-header, .copy-features-header, .copy-trading-platform-header, .copy-how-it-work-section',  slideUp);
